e0e5a728d2884e6a0387a778ab6f85975762f08a
seventapple <903159351@qq.com>
2021-05-12 20:41:05 +0800

    Mod:三重排序规则新规

--- src/main/java/com/wang/tool/ClassCompareTest.java
+++ src/main/java/com/wang/tool/ClassCompareTest.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public class ClassCompareTest {
 	public static void main(String[] args) {
+		//Comparable
 		sort1();
+        //Comparator
 		sort2();
+		//三重判断
+		sort3();
 	}
 
 	// 需求:flag唯一为true,此情况序列为集合最后
@@ -52,46 +56,68 @@ public class ClassCompareTest {
 		System.out.println("排序后");
 		System.out.println(list);
 	}
+
+	// 需求:lastFlg唯一为true,此情况序列为集合最后
+	// lastFlgEver复数情况为true,第二优先级别靠后,同级别按时间升顺
+	// date按时间升顺(null最前)
+	public static void sort3() {
+		CompareThree c1 = new CompareThree("c1", false, false, null);
+		CompareThree c2 = new CompareThree("c2", false, false, "20200101");
+		CompareThree c3 = new CompareThree("c3", false, false, "20200125");
+		CompareThree c4 = new CompareThree("c4", false, true, "20200119");
+		CompareThree c5 = new CompareThree("c5", false, true, "20200120");
+		CompareThree c6 = new CompareThree("c6", false, true, "20200131");
+		CompareThree c7 = new CompareThree("c7", true, true, "20200130");
+		List<CompareThree> list = new ArrayList<CompareThree>();
+		list.add(c6);
+		list.add(c3);
+		list.add(c1);
+		list.add(c7);
+		list.add(c5);
+		list.add(c4);
+		list.add(c2);
+		System.out.println("排序前");
+		System.out.println(list);
+		Collections.sort(list);
+		System.out.println("排序后");
+		System.out.println(list);
+	}
 }
 
-class CompareOne implements Comparable<CompareOne> {
-
-	public CompareOne(boolean flag, String date) {
-		super();
-		this.flag = flag;
-		this.date = date;
-	}
-
-	private boolean flag;
+class CompareThree implements Comparable<CompareThree> {
+	private String name;
+	private boolean lastFlg;
+	private boolean lastFlgEver;
 	private String date;
 
-	public boolean isFlag() {
-		return flag;
-	}
-
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
+	public CompareThree(String name, boolean lastFlg, boolean lastFlgEver, String date) {
+		super();
+		this.name = name;
+		this.lastFlg = lastFlg;
+		this.lastFlgEver = lastFlgEver;
 		this.date = date;
 	}
 
 	@Override
 	public String toString() {
-		return flag + " + " + date;
+		return name;
 	}
 
 	@Override
-	public int compareTo(CompareOne bean) {
-		if (this.flag) {
+	public int compareTo(CompareThree bean) {
+		if (this.lastFlg) {
 			return 1;
 		}
-		if (bean.flag) {
+		if (bean.lastFlg) {
+			return -1;
+		}
+		if (this.lastFlgEver && bean.lastFlgEver) {
+			return this.date.compareTo(bean.date);
+		}
+		if (this.lastFlgEver) {
+			return 1;
+		}
+		if (bean.lastFlgEver) {
 			return -1;
 		}
 		if (this.date == null) {
@@ -157,4 +183,55 @@ class CompareTwo {
 	public String toString() {
 		return flag + " + " + date;
 	}
+}
+
+class CompareOne implements Comparable<CompareOne> {
+
+	public CompareOne(boolean flag, String date) {
+		super();
+		this.flag = flag;
+		this.date = date;
+	}
+
+	private boolean flag;
+	private String date;
+
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return flag + " + " + date;
+	}
+
+	@Override
+	public int compareTo(CompareOne bean) {
+		if (this.flag) {
+			return 1;
+		}
+		if (bean.flag) {
+			return -1;
+		}
+		if (this.date == null) {
+			return -1;
+		}
+		if (bean.date == null) {
+			return 1;
+		}
+		return this.date.compareTo(bean.date);
+	}
+
 }
