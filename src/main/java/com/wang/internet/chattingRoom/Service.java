c8cf5204935ca0073402521ed6d2869361cc1729
seventapple <903159351@qq.com>
2021-05-25 20:51:46 +0800

    FEAT:Socket 聊天室-1 服务器的搭建 实现功能 将用户发言转发给其他的用户

--- src/main/java/com/wang/internet/chattingRoom/Service.java
+++ src/main/java/com/wang/internet/chattingRoom/Service.java
@@ -8,31 +8,31 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+
+import com.sun.istack.internal.Pool;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import com.wang.io.FileUtil;
 
 //聊天室
 //需求与功能:
 //1.服务器负责各个客户端的消息转发
 //2.每个客户端有单独的信息发送和接收功能
 public class Service {
+	public ArrayList<MyChannel> pool = new ArrayList<MyChannel>();
 
 	public static void main(String[] args) {
+		new Service().start();
+	}
+
+	public void start() {
 		try (// 创建服务器,指定端口(1024以下系统占用)
 				ServerSocket server = new ServerSocket(8888);) {// 接收客服端连接 阻塞式
 			while (true) {
 				Socket accept = server.accept();
-//				new Thread(new Send(accept)).start();
-//				new Thread(new Receive(accept)).start();
-				while (true) {
-					OutputStream os = accept.getOutputStream();
-					// 4.接收客户端数据
-					DataInputStream dis = new DataInputStream(accept.getInputStream());
-					String readUTF = dis.readUTF();
-					System.out.println("get msg from client : " + readUTF);
-					// 3.发送数据to客服端
-					DataOutputStream dos = new DataOutputStream(accept.getOutputStream());
-					dos.writeUTF("Service->" + readUTF);
-					dos.flush();
-				}
+				MyChannel channel = new MyChannel(accept);
+				pool.add(channel);
+				new Thread(channel).start();
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -40,4 +40,61 @@ public class Service {
 		}
 	}
 
+	private class MyChannel implements Runnable {
+
+		private DataInputStream dis;
+		private DataOutputStream dos;
+		private boolean isRunning = true;
+
+		public MyChannel(Socket client) {
+			try {
+				dis = new DataInputStream(client.getInputStream());
+				dos = new DataOutputStream(client.getOutputStream());
+			} catch (IOException e) {
+				isRunning = false;
+				pool.remove(this);
+				FileUtil.close(dis, dos);
+			}
+		}
+
+		private String getMsg() {
+			String msgString = "";
+			try {
+				msgString = dis.readUTF();
+			} catch (IOException e) {
+				isRunning = false;
+				pool.remove(this);
+				FileUtil.close(dis, dos);
+			}
+			return msgString;
+		}
+
+		private void Send(String msg) {
+			try {
+				dos.writeUTF(msg);
+				dos.flush();
+			} catch (IOException e) {
+				isRunning = false;
+				pool.remove(this);
+				FileUtil.close(dis, dos);
+			}
+		}
+
+		private void SendOthers(String msg) {
+			for (MyChannel chl : pool) {
+				if (chl == this) {
+					continue;
+				}
+				chl.Send(msg);
+			}
+		}
+
+		@Override
+		public void run() {
+			while (isRunning) {
+				SendOthers(getMsg());
+			}
+		}
+
+	}
 }
