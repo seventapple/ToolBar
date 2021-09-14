caa483ffa389175fd2fd319bb6c12ad4f89a612f
guoguang.wang <903159351@qq.com>
2021-09-14 20:57:08 +0800

    FEAT:Socket通信 设置通信等待时间

--- src/main/java/com/wang/internet/multiScoket/Client3.java
+++ src/main/java/com/wang/internet/multiScoket/Client3.java
@@ -5,22 +5,26 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Client {
+public class Client3 {
 
 	public static void main(String[] args) {
-		try (// 1.创建客户端 服务器+端口 立即开始连接
-				Socket client = new Socket("127.0.0.1", 8888);
-				// 2.接收服务器数据
-				InputStream is = client.getInputStream();) {
-			client.setSoTimeout(3000);
+		try {
+			// 1.创建客户端 服务器+端口 立即开始连接
+			Socket testSet = new Socket();
+			InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8888);
+			testSet.connect(address, 10000);
+			testSet.setSoTimeout(3000);
+			// 2.接收服务器数据
+			InputStream is = testSet.getInputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 			String readLine = br.readLine();
 			System.out.println(readLine);
 			// 3.发送数据to服务器
-			DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+			DataOutputStream dos = new DataOutputStream(testSet.getOutputStream());
 			dos.writeUTF("发给服务器的信息.");
 			dos.flush();
 		} catch (UnknownHostException e) {
