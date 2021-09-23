fcb5ab354f6e1ed96e5b1d80ccb582086b713b0f
guoguang.wang <903159351@qq.com>
2021-09-23 20:28:48 +0800

    FIX:classloader 复习

--- src/main/java/com/wang/classloader/ClassLoaderTest.java
+++ src/main/java/com/wang/classloader/ClassLoaderTest.java
@@ -2,11 +2,30 @@ package com.wang.classloader;
 
 public class ClassLoaderTest {
 	public void base() {
+		// -Xbootclasspath/a:E:\001Git\BatRun\bat1\lib\bat1.jar
+		System.out.println("1.Bootstrap ClassLoader:");
 		System.out.println(System.getProperty("sun.boot.class.path"));
+		/**
+		 * java命令引入jar时可以-cp参数，但-cp只能指定一个固定jar包，不能用通配符(多个jar时要一个个写,不能*.jar)，
+		 * 通常情况jar都在同一目录，且多于1个。可以使用-Djava.ext.dirs。
+		 * -Djava.ext.dirs=E:\Java\jre1.8.0_25\lib\ext
+		 * 可以多个指定(-Djava.ext.dirs="libs\;%JAVA_HOME%\jre\lib\ext;E:\001Git\BatRun\bat1\lib\bat1.jar")
+		 */
+		System.out.println("2.Extention ClassLoader:");
 		System.out.println(System.getProperty("java.ext.dirs"));
+		System.out.println("3.AppClassLoader:");
 		System.out.println(System.getProperty("java.class.path"));
+//		try {
+//			Class.forName("com.bat.StartUp");
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
 		ClassLoader cl = ClassLoaderTest.class.getClassLoader();
 		System.out.println("ClassLoader is : " + cl.toString());
 		System.out.println("ClassLoader' parent is : " + cl.getParent().toString());
+	}
+
+	public static void main(String[] args) {
+			new ClassLoaderTest().base();
 	}
 }
--- src/main/java/com/wang/classloader/RunFileLoader.java
+++ src/main/java/com/wang/classloader/RunFileLoader.java
@@ -1,9 +1,18 @@
 package com.wang.classloader;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class RunFileLoader {
 
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) throws Exception {
 		FileClassLoader fcl = new FileClassLoader("E:/TestFile/class");
+		System.out.println("===读取class文件,调用其方法say()===");
+		Class<?> loadClass = fcl.loadClass("com.wang.cl.Hello");
+		Object obj = loadClass.newInstance();
+		Method method = loadClass.getDeclaredMethod("say", null);
+		method.invoke(obj, null);
+		System.out.println("===读取对象比较,使用一个CL类(单例实现)===");
 		Class<?> c = fcl.findClass("com.wang.cl.Hello");
 		Class<?> c2 = fcl.findClass("com.wang.cl.Hello");
 		System.out.println(c.hashCode());
@@ -11,11 +20,14 @@ public class RunFileLoader {
 		FileClassLoader fcl2 = new FileClassLoader("E:/TestFile/class");
 		Class<?> c3 = fcl2.findClass("com.wang.cl.Hello");
 		Class<?> c4 = fcl2.findClass("java.lang.String");
+		System.out.println("===读取对象比较,两个CL类===");
 		System.out.println(c3.hashCode());
 		// 查看类由谁加载
+		System.out.println("===查看类===");
 		System.out.println(c3.getClassLoader());// 自定义
 		System.out.println(c4.getClassLoader());// 引导BootstrapClassLoader
 		// 直接加载加密的类
+		System.out.println("===直接读取加密类===");
 		try {
 			Class<?> c5 = fcl.findClass("com.wang.cl.HelloEncrpy");
 			System.out.println(c5);
@@ -24,11 +36,14 @@ public class RunFileLoader {
 		}
 		// 对加密的类使用自定义类加载器加载
 		DecrpyClassLoader dLoader = new DecrpyClassLoader("E:/TestFile/class");
+		System.out.println("===读取加密类===");
 		Class<?> dc = dLoader.findClass("com.wang.cl.HelloEncrpy");
 		System.out.println(dc);
 		// 线程上下文类加载器
+		System.out.println("===线程上下文类加载器===");
 		System.out.println("current thread cl : " + Thread.currentThread().getContextClassLoader());
 		// 设置线程的类加载器(通过设置自定义加载器,可抛弃双亲委派加载链模式)
+		System.out.println("===设置线程的类加载器===");
 		Thread.currentThread().setContextClassLoader(dLoader);
 		System.out.println("update thread cl : " + Thread.currentThread().getContextClassLoader());
 		Class<?> c6 = Thread.currentThread().getContextClassLoader().loadClass("com.wang.cl.HelloEncrpy");
